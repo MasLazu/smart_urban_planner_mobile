3a31f3e08d0d327d35aae69d6f77f6ccc68813bc
mfaziz <mfaziz50@gmail.com>
2024-03-31 16:48:09 +0700

    feat: slicing feed page

--- lib/modules/home/controller.dart
+++ lib/modules/home/controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:smart_urban_planner/core/theme/styles.dart';
 import 'package:smart_urban_planner/modules/explore/view.dart';
+import 'package:smart_urban_planner/modules/feed/view.dart';
 import 'package:smart_urban_planner/modules/profile/view.dart';
 
 class HomeController extends GetxController {
@@ -12,7 +13,7 @@ class HomeController extends GetxController {
   HomeController() {
     _tabContents.addAll([
       ExploreView(),
-      Container(color: Styles.backgroundColor),
+      const FeedView(),
       Container(color: Styles.backgroundColor),
       const ProfileView(),
     ]);
