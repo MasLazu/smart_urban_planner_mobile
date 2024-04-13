cb3e86c0b063c3933979a40d83d948cf2acb9d02
mfaziz <mfaziz50@gmail.com>
2024-04-13 17:50:35 +0700

    fix: auth page layout

--- lib/modules/login/view.dart
+++ lib/modules/login/view.dart
@@ -14,94 +14,96 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Form(
-            key: _controller.formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Sign In",
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _controller.formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Sign In",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  "Welcome back! Please sign in to your account.",
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                const SizedBox(height: 32),
-                Text(
-                  'Email',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                const SizedBox(height: 8),
-                FormInput(
-                  controller: _controller.emailController,
-                  hintText: 'Enter your email address',
-                  isPassword: false,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Email is required';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  'Password',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                const SizedBox(height: 8),
-                FormInput(
-                  controller: _controller.passwordController,
-                  hintText: 'Enter your password',
-                  isPassword: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Password is required';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 42),
-                ElevatedButton(
-                  onPressed: () => _controller.submit(),
-                  style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all(
-                        const Size(double.infinity, 48)),
-                    backgroundColor:
-                        MaterialStateProperty.all(Styles.primaryColor),
+                  const SizedBox(height: 8),
+                  Text(
+                    "Welcome back! Please sign in to your account.",
+                    style: Theme.of(context).textTheme.bodyLarge,
                   ),
-                  child: Obx(
-                    () => _controller.isLoading.value
-                        ? CircularProgressIndicator(
-                            color: Styles.secondaryBackgroundColor)
-                        : const Text('Sign In'),
+                  const SizedBox(height: 32),
+                  Text(
+                    'Email',
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Don't have an account?"),
-                    TextButton(
-                      onPressed: () {
-                        Get.offAllNamed(RouteNames.register);
-                      },
-                      child: Text(
-                        'Sign up',
-                        style: TextStyle(
-                          color: Styles.primaryColor,
-                          fontWeight: FontWeight.bold,
+                  const SizedBox(height: 8),
+                  FormInput(
+                    controller: _controller.emailController,
+                    hintText: 'Enter your email address',
+                    isPassword: false,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Email is required';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Password',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 8),
+                  FormInput(
+                    controller: _controller.passwordController,
+                    hintText: 'Enter your password',
+                    isPassword: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Password is required';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 42),
+                  ElevatedButton(
+                    onPressed: () => _controller.submit(),
+                    style: ButtonStyle(
+                      minimumSize: MaterialStateProperty.all(
+                          const Size(double.infinity, 48)),
+                      backgroundColor:
+                          MaterialStateProperty.all(Styles.primaryColor),
+                    ),
+                    child: Obx(
+                      () => _controller.isLoading.value
+                          ? CircularProgressIndicator(
+                              color: Styles.secondaryBackgroundColor)
+                          : const Text('Sign In'),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Don't have an account?"),
+                      TextButton(
+                        onPressed: () {
+                          Get.offAllNamed(RouteNames.register);
+                        },
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(
+                            color: Styles.primaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/modules/register/view.dart
+++ lib/modules/register/view.dart
@@ -14,111 +14,113 @@ class RegisterView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Form(
-            key: _controller.formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Sign Up",
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _controller.formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  "Welcome! Please sign up to create an account.",
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                const SizedBox(height: 32),
-                Text(
-                  'Name',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                const SizedBox(height: 8),
-                FormInput(
-                  controller: _controller.nameController,
-                  hintText: 'Enter your name',
-                  isPassword: false,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Password is required';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  'Email',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                const SizedBox(height: 8),
-                FormInput(
-                  controller: _controller.emailController,
-                  hintText: 'Enter your email address',
-                  isPassword: false,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Email is required';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  'Password',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                const SizedBox(height: 8),
-                FormInput(
-                  controller: _controller.passwordController,
-                  hintText: 'Enter your password',
-                  isPassword: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Password is required';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 42),
-                ElevatedButton(
-                  onPressed: () => _controller.submit(),
-                  style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all(
-                        const Size(double.infinity, 48)),
-                    backgroundColor:
-                        MaterialStateProperty.all(Styles.primaryColor),
+                  const SizedBox(height: 8),
+                  Text(
+                    "Welcome! Please sign up to create an account.",
+                    style: Theme.of(context).textTheme.bodyLarge,
                   ),
-                  child: Obx(
-                    () => _controller.isLoading.value
-                        ? CircularProgressIndicator(
-                            color: Styles.secondaryBackgroundColor)
-                        : const Text('Sign Up'),
+                  const SizedBox(height: 32),
+                  Text(
+                    'Name',
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Already have an account?"),
-                    TextButton(
-                      onPressed: () {
-                        Get.offAllNamed(RouteNames.login);
-                      },
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          color: Styles.primaryColor,
-                          fontWeight: FontWeight.bold,
+                  const SizedBox(height: 8),
+                  FormInput(
+                    controller: _controller.nameController,
+                    hintText: 'Enter your name',
+                    isPassword: false,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Password is required';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Email',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 8),
+                  FormInput(
+                    controller: _controller.emailController,
+                    hintText: 'Enter your email address',
+                    isPassword: false,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Email is required';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Password',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 8),
+                  FormInput(
+                    controller: _controller.passwordController,
+                    hintText: 'Enter your password',
+                    isPassword: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Password is required';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 42),
+                  ElevatedButton(
+                    onPressed: () => _controller.submit(),
+                    style: ButtonStyle(
+                      minimumSize: MaterialStateProperty.all(
+                          const Size(double.infinity, 48)),
+                      backgroundColor:
+                          MaterialStateProperty.all(Styles.primaryColor),
+                    ),
+                    child: Obx(
+                      () => _controller.isLoading.value
+                          ? CircularProgressIndicator(
+                              color: Styles.secondaryBackgroundColor)
+                          : const Text('Sign Up'),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Already have an account?"),
+                      TextButton(
+                        onPressed: () {
+                          Get.offAllNamed(RouteNames.login);
+                        },
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            color: Styles.primaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
